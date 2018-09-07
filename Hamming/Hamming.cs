04c8be4525388556df3e42c6849fbe331f071056
Narendar kumar <nkumar010896@gmail.com>
2018-09-07 17:05:38 +0530

    Update Hamming.cs

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -1,23 +1,15 @@
 using System;
-class Program{
-static void Main(){
-     Console.WriteLine("enter first string");
-     string str1=Console.ReadLine();
-     Console.WriteLine("enter second string");
-     string str2=Console.ReadLine();  
-       int k=0;
-       int i=0;
-       int j=str1.Length;
-     while(i<j)
-     {
-        if(str1[i]!=str2[i])
-         {
-             k++;
-         }
-         
-         i++;
-     }
-    Console.WriteLine(k);
- }
+
+namespace Hamming
+{
+    public class Hamming
+    {
+        public static int Distance(string original, string current)
+        {
+            int hammingDistance = 0;
+
+            return hammingDistance;
+        }
+    }
 }
  
