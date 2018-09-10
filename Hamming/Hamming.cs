46ec0be80faf675d14dbe746e5e231303aabd21e
narendar kumar <nkumar010896@gmail.com>
2018-09-10 15:16:41 +0530

    SDV

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -6,9 +6,31 @@ namespace Hamming
     {
         public static int Distance(string original, string current)
         {
-            int hammingDistance = 0;
-
-            return hammingDistance;
+       if(original==null)
+        {
+          throw(new ArgumentNullException("original"));
+        }
+        if(current==null)
+        {
+         throw(new ArgumentNullException("current"));
+        }
+        if(original.Length!=current.Length)
+        {
+            throw(new ArgumentException("Hamming Distance can only be calculated over strings of equal length"));
+        }
+        int hammingDistance = 0;
+        int s=original.Length;
+        int i=0;
+            
+        while(i<s)
+            {
+                if(original[i]!=current[i])
+                {
+                    hammingDistance=hammingDistance+1;
+                }
+                i++;
+            }
+         return hammingDistance;
         }
     }
 }
